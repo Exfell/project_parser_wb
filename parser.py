c2259c78aeeea370a81a510c9ba7468f2d0726da
Exfell <100291975+Exfell@users.noreply.github.com>
2024-12-12 10:09:24 +0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -46,7 +46,8 @@ def search_category_in_catalog(url: str, catalog_list: list) -> dict:
             return catalog
 
 
-def get_data_from_json(json_file: dict, min_rating: float = 0.0, low_price: int = 1, top_price: int = 1000000) -> list:
+def get_data_from_json(json_file: dict, min_rating: float = 0.0, low_price: int = 1, top_price: int = 1000000,
+                       keywords: list = None) -> list:
     data_list = []
     for data in json_file['data']['products']:
         # Используем цену со скидкой, если она есть, иначе - обычную цену
@@ -56,10 +57,15 @@ def get_data_from_json(json_file: dict, min_rating: float = 0.0, low_price: int 
 
         # Проверка фильтров: по рейтингу и цене
         if reviewRating >= min_rating and low_price <= salePriceU <= top_price:
+            # Проверка ключевых слов в названии товара
+            if keywords and not any(keyword.lower() in data.get('name', '').lower() for keyword in keywords):
+                continue  # Пропускаем товар, если ни одно ключевое слово не найдено
+
+            # Если все проверки прошли, добавляем товар в список
             data_list.append({
                 'id': data.get('id'),
                 'name': data.get('name'),
-                'salePriceU': salePriceU,  # Явно добавляем цену со скидкой
+                'salePriceU': salePriceU,
                 'cashback': data.get('feedbackPoints'),
                 'sale': data.get('sale'),
                 'brand': data.get('brand'),
@@ -71,7 +77,6 @@ def get_data_from_json(json_file: dict, min_rating: float = 0.0, low_price: int 
                 'link': f'https://www.wildberries.ru/catalog/{data.get("id")}/detail.aspx?targetUrl=BP'
             })
     return data_list
-
 
 @retry(Exception, tries=-1, delay=0)
 def scrap_page(page: int, shard: str, query: str, low_price: int, top_price: int, discount: int = None, rating: int = 0) -> dict:
@@ -91,15 +96,16 @@ def scrap_page(page: int, shard: str, query: str, low_price: int, top_price: int
     return r.json()
 
 
-def parser(url: str, low_price: int = 1, top_price: int = 1000000, discount: int = 0, min_rating: float = 0):
-    """Основная функция с фильтрацией по минимальному рейтингу"""
+
+def parser(url: str, low_price: int = 1, top_price: int = 1000000, discount: int = 0, min_rating: float = 0, keywords: list = None):
+    """Основная функция с фильтрацией по ключевым словам"""
     catalog_data = get_data_category(get_catalogs_wb())
     try:
         # Поиск введенной категории в общем каталоге
         category = search_category_in_catalog(url=url, catalog_list=catalog_data)
         data_list = []
 
-        for page in range(1, 51):  # вб отдает 50 страниц товара (раньше было 100)
+        for page in range(1, 51):  # ВБ отдает максимум 50 страниц товара
             data = scrap_page(
                 page=page,
                 shard=category['shard'],
@@ -109,8 +115,8 @@ def parser(url: str, low_price: int = 1, top_price: int = 1000000, discount: int
                 discount=discount
             )
 
-            # Передаем минимальный рейтинг для фильтрации товаров
-            extracted_data = get_data_from_json(data, min_rating=min_rating)
+            # Передаем минимальный рейтинг и ключевые слова для фильтрации товаров
+            extracted_data = get_data_from_json(data, min_rating=min_rating, low_price=low_price, top_price=top_price, keywords=keywords)
             print(f'Добавлено позиций: {len(extracted_data)}')
 
             if extracted_data:
@@ -119,7 +125,7 @@ def parser(url: str, low_price: int = 1, top_price: int = 1000000, discount: int
                 break
 
         print(f'Сбор данных завершен. Собрано: {len(data_list)} товаров.')
-        save_excel(data_list, f'{category["name"]}_from_{low_price}_to_{top_price}_rating_{min_rating}')
+        save_excel(data_list, f'{category["name"]}_from_{low_price}_to_{top_price}_rating_{min_rating}_keywords')
         print(
             f'Ссылка для проверки: {url}?priceU={low_price * 100};{top_price * 100}&discount={discount}&minRating={min_rating}')
 
@@ -151,17 +157,17 @@ def save_excel(data: list, filename: str):
     print(f'Все сохранено в {filename}.xlsx\n')
 
 if __name__ == '__main__':
-    """данные для теста. собераем товар с раздела велосипеды в ценовой категории от 1тыс, до 100тыс, со скидкой 10%"""
-    # url = 'https://www.wildberries.ru/catalog/sport/vidy-sporta/velosport/velosipedy'
     while True:
         try:
             url = input('Введите ссылку на категорию без фильтров для сбора (или "q" для выхода):\n')
             if url == 'q':
                 break
+            keywords_input = input('Введите ключевые слова для фильтрации (через пробел, или просто нажмите Enter):')
             low_price = int(input('Введите минимальную сумму товара: '))
             top_price = int(input('Введите максимальную сумму товара: '))
             discount = int(input('Введите минимальную скидку (введите 0 если без скидки): '))
             min_rating = float(input('Введите минимальную оценку (введите 0 для любого рейтинга): '))
-            parser(url=url, low_price=low_price, top_price=top_price, discount=discount, min_rating=min_rating)
+            keywords = [word.strip() for word in keywords_input.split()] if keywords_input else []
+            parser(url=url, low_price=low_price, top_price=top_price, discount=discount, min_rating=min_rating, keywords=keywords)
         except ValueError:
             print('Ошибка ввода! Проверьте, что все введенные данные являются числами.\nПерезапуск...')
