1881277c17b47df27c6462f35f3a4887aa607224
Exfell <100291975+Exfell@users.noreply.github.com>
2025-01-16 18:01:23 +0500

    Create back.py

---
+++ back.py
@@ -0,0 +1,50 @@
+from quart import Quart, request, jsonify, send_file
+from quart_cors import cors
+import os
+import asyncio
+from parser import parser
+import logging
+
+# Отключаем информацию от asyncio
+logging.getLogger('asyncio').setLevel(logging.CRITICAL)
+app = Quart(__name__)
+cors(app)  # Для разрешения запросов с фронтенда
+
+@app.route('/api/parse', methods=['POST'])
+async def api_parse():
+    try:
+        data = await request.json
+        keywords = data.get('keywords', '')
+        low_price = int(data.get('low_price', 1))
+        top_price = int(data.get('top_price', 1000000))
+        discount = int(data.get('discount', 0))
+        min_rating = float(data.get('min_rating', 0))
+        # Вызов парсера
+        filename = await parser(keywords, low_price, top_price, discount, min_rating) # Используйте await
+        return jsonify({"message": "Парсинг завершен успешно!", "file": filename})
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+
+
+@app.route('/download/<filename>', methods=['GET'])
+async def download(filename):
+    file_path = os.path.join('.', filename)
+    print('Зашёл', file_path)
+    if not os.path.exists(file_path):
+        return jsonify({"message": "Файл не найден"}), 404
+
+    try:
+        # Отправляем файл
+        response = await send_file(file_path, as_attachment=True)
+        print('Работает')
+
+        # Удаляем файл после отправки через 5 секунд
+
+        return response
+
+    except Exception as e:
+        return jsonify({"message": f"Ошибка при отправке файла: {e}"}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
